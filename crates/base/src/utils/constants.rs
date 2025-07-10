143e15070130b398241644f234638f5ec7ea011c
the-db-architect <the-db-architect@users.noreply.github.com>
2025-07-09 23:26:20 -0400

    chore: Change endpoint

--- crates/base/src/utils/constants.rs
+++ crates/base/src/utils/constants.rs
@@ -3,5 +3,5 @@ use std::sync::LazyLock;
 pub const WVM_RPC_URL: &str = "https://alphanet.load.network";
 
 pub static UPLOADER: LazyLock<String> = LazyLock::new(|| {
-    std::env::var("UPLOADER_API").unwrap_or_else(|_| "https://uploader.load.network".to_string())
+    std::env::var("UPLOADER_API").unwrap_or_else(|_| "https://k8s.turbo-uploader-service.load.network".to_string())
 });
