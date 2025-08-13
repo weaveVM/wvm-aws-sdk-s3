8cdc4a9af50145b62d95fe591e3cc57c513ce679
the-db-architect <the-db-architect@users.noreply.github.com>
2025-08-12 20:38:13 -0400

    chore: Change auth endpoint

--- server/src/utils/vars.rs
+++ server/src/utils/vars.rs
@@ -1,7 +1,7 @@
 use std::sync::LazyLock;
 
 pub static AUTH_HOST: LazyLock<String> = LazyLock::new(|| {
-    std::env::var("AUTH_API").unwrap_or_else(|_| "https://load-auth-vskt.shuttle.app".to_string())
+    std::env::var("AUTH_API").unwrap_or_else(|_| "https://k8s.load-auth-service.load.network".to_string())
 });
 
 pub static DATA_RETRIEVAL_ENDPOINT: LazyLock<String> = LazyLock::new(|| {
